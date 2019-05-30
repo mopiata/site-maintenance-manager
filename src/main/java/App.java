12682f6ff3123a61d6c27faec5fd23d851ad8626
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-30 13:31:44 +0300

    styled index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
 
+        get("/", (request, response) -> {
+            Map<String, Object> model=new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
