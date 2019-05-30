83b56b4e8d1490313fdec8bf295683fcc272c060
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-05-30 11:27:04 +0300

    created sites class

---
+++ src/main/java/models/Sites.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Sites {
+    private int id;
+    private String siteName;
+    private String siteLocation;
+    private LocalDateTime createdAt;
+
+    public Sites(String name, String location){
+        this.siteName=name;
+        this.siteLocation=location;
+        this.createdAt=LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return siteName;
+    }
+
+    public void setName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getLocation() {
+        return siteLocation;
+    }
+
+    public void setLocation(String siteLocation) {
+        this.siteLocation = siteLocation;
+    }
+
+    public LocalDateTime getCreated() {
+        return createdAt;
+    }
+
+    public void setCreated(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sites sites = (Sites) o;
+        return Objects.equals(siteName, sites.siteName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(siteName);
+    }
+}
